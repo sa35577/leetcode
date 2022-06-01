4b568a676656704883109b703008fb819d8644f8
sa35577 <38817928+sa35577@users.noreply.github.com>
2022-06-01 15:21:33 +0000

    Sync LeetCode submission - Two Sum (javascript)

---
+++ solutions/problems/two_sum/solution.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = 0; j < i; j++) {
+            if (nums[i] + nums[j] == target) {
+                let res = [i, j];
+                return res;
+            }
+            
+        }
+    }
+};
