abd753cd6b26f452e7d0bcd9a7fc9177e627db06
sa35577 <38817928+sa35577@users.noreply.github.com>
2022-09-07 13:23:36 +0000

    Sync LeetCode submission - Longest Substring Without Repeating Characters (cpp)

--- solutions/problems/longest_substring_without_repeating_characters/solution.cpp
+++ solutions/problems/longest_substring_without_repeating_characters/solution.cpp
@@ -1,24 +1,22 @@
-#include <unordered_set>
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        if (s.size() <= 1) return s.size();
-        int L = 0, R = 0;
-        int mx = -1;
-        unordered_set<char> st;
-        while (R < s.size()) {
-            st.insert(s[R]);
-            if (st.size() < R-L+1) {
-                while (L < R && s[R] != s[L]) {
-                    st.erase(s[L]);
-                    L++;
-                }
-                if (L < R && s[L] == s[R]) {
-                    L++;
-                }
+        set<char> ss;
+        int leftptr = 0, rightptr = leftptr, mx = 0;
+        while (rightptr < s.size()) {
+            if (ss.count(s[rightptr]) == 0) {
+                mx = max(mx,rightptr-leftptr+1);
+                ss.insert(s[rightptr++]);
             }
-            mx = max(mx,R-L+1);
-            R++;
+            else {
+                while (s[leftptr] != s[rightptr]) {
+                    ss.erase(s[leftptr++]);
+                }
+                ss.erase(s[leftptr++]);
+                mx = max(mx,rightptr-leftptr+1);
+                ss.insert(s[rightptr++]);
+            }
+            //printf("%d %d %d\n", leftptr, rightptr, mx);
         }
         return mx;
     }
