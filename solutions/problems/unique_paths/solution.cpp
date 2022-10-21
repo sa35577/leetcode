a371756c44638e7fba21b464109fee71e183039e
sa35577 <38817928+sa35577@users.noreply.github.com>
2022-10-21 16:44:45 +0000

    Sync LeetCode submission - Unique Paths (cpp)

--- solutions/problems/unique_paths/solution.cpp
+++ solutions/problems/unique_paths/solution.cpp
@@ -1,14 +1,34 @@
 class Solution {
 public:
     int uniquePaths(int m, int n) {
-        int dp[101][101];
-        dp[0][0] = 0;
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (i == 0 || j == 0) dp[i][j] = 1;
-                else dp[i][j] = dp[i-1][j] + dp[i][j-1];
+        int dp[100][100];
+        for (int i = 0; i < 100; i++) {
+            dp[i][0] = 1;
+            dp[0][i] = 1;
+        }
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                dp[i][j] = dp[i-1][j] + dp[i][j-1];
             }
         }
         return dp[m-1][n-1];
+        // m--;
+        // n--;
+        // if (m+n == 0) return 1;
+        // //m+n choose m = (m+n)!/m!*n!
+        // long long num = 1;
+        // long long denom = 1;
+        // for (int i = max(m,n)+1; i <= m+n; i++) {
+        //     long long x = (long long)(i);
+        //     num *= x;
+        // }
+        // for (int i = min(m,n); i >= 1; i--) {
+        //     long long x = (long long)(i);
+        //     denom *= x;
+        // }
+        // long long res = num / denom;
+        // return (int)(res);
+
+
     }
 };
