cfb4caeedc5b05763fa4155f3427e80c40e2f64b
sa35577 <38817928+sa35577@users.noreply.github.com>
2023-09-21 18:47:14 +0000

    Sync LeetCode submission - Insert Interval (cpp)

--- solutions/problems/insert_interval/solution.cpp
+++ solutions/problems/insert_interval/solution.cpp
@@ -1,35 +1,39 @@
 class Solution {
 public:
-    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
-        vector<vector<int>> v;
-        int curPos = 0;
-        while (curPos < intervals.size() && intervals[curPos][1] < newInterval[0]) {
-            v.push_back(intervals[curPos++]);
-        }
-        if (curPos == intervals.size()) {
-            v.push_back(newInterval);
-            return v;
-        }
-        int L = 0,R = 0;
-        if (intervals[curPos][0] < newInterval[0]) { //build with current interval in array as left end
-            L = intervals[curPos][0];
-            R = max(intervals[curPos][1],newInterval[1]);
-            while (curPos < intervals.size() && intervals[curPos][0] <= R) {
-                R = max(R,intervals[curPos++][1]);
-            }
-        }
-        else { //build with new interval as left end
-            L = newInterval[0];
-            R = newInterval[1];
-            while (curPos < intervals.size() && intervals[curPos][0] <= R) {
-                R = max(R,intervals[curPos++][1]);
-            }
-        }
-        vector<int> newElement{L,R};
-        v.push_back(newElement);
-        while (curPos < intervals.size()) {
-                v.push_back(intervals[curPos++]);
-        }
+    bool intersects(vector<int> &a, vector<int> &b) {
+        if (a[1] < b[0] || b[1] < a[0]) return false;
+        return true;
+    }
+    vector<int> unite(vector<int> &a, vector<int> &b) {
+        vector<int> v;
+        if (!intersects(a,b)) return v;
+        v.push_back(min(a[0],b[0]));
+        v.push_back(max(a[1],b[1]));
         return v;
+    }
+    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
+        // + 2.5 mins
+        vector<vector<int>> newIntervals;
+        int idx = 0;
+        while (idx < intervals.size() && intervals[idx][1] < newInterval[0]) {
+            newIntervals.push_back(intervals[idx]);
+            idx++;
+        }
+        // cout << idx << endl;
+        
+        while (idx < intervals.size() && intersects(intervals[idx],newInterval)) {
+            newInterval = unite(newInterval,intervals[idx++]);
+        }
+        
+        newIntervals.push_back(newInterval);
+        while (idx < intervals.size()) {
+            newIntervals.push_back(intervals[idx]);
+            idx++;
+        }
+
+        return newIntervals;
+
+
+
     }
 };
