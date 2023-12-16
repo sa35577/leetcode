61ef842a91950fd2826271551fe3dba446507e88
sa35577 <38817928+sa35577@users.noreply.github.com>
2023-12-16 05:25:08 +0000

    Sync LeetCode submission - Minimum Deletions to Make Array Divisible (cpp)

---
+++ solutions/problems/minimum_deletions_to_make_array_divisible/solution.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int minOperations(vector<int>& nums, vector<int>& numsDivide) {
+        int g = numsDivide[0];
+        for (int i = 1; i < numsDivide.size(); i++) g = gcd(g,numsDivide[i]);
+        sort(nums.begin(),nums.end());
+        for (int i = 0; i < nums.size(); i++) {
+            if (g % nums[i] == 0) return i;
+        }
+        return -1;
+    }
+};
