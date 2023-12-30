2fcf3129f48389ca008fd2c3a71d7d30386f0125
sa35577 <38817928+sa35577@users.noreply.github.com>
2023-12-30 13:14:36 +0000

    Sync LeetCode submission - Coin Change (cpp)

--- solutions/problems/coin_change/solution.cpp
+++ solutions/problems/coin_change/solution.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
     int coinChange(vector<int>& coins, int amount) {
-        vector<long long> dp(amount+1);
+        int dp[10001];
+        fill(dp,dp+10001,INT_MAX);
         dp[0] = 0;
-        for (int i = 1; i < dp.size(); i++) {
-            dp[i] = LLONG_MAX;
-            for (int j : coins) {
-                if (i-j >= 0 && dp[i-j] != LLONG_MAX) {
-                    dp[i] = min(dp[i],dp[i-j]+1);
+        for (int i = 1; i <= amount; i++) {
+            for (int coin : coins) {
+                if (i - coin >= 0 && dp[i-coin] != INT_MAX) {
+                    dp[i] = min(dp[i],dp[i-coin]+1);
                 }
             }
         }
-        return (int)dp.back();
-        
+        if (dp[amount] == INT_MAX) return -1; 
+        return dp[amount];
     }
 };
