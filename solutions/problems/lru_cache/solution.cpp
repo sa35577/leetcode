27a5e8b6352075899a7f6f78d7dde827bf6e88c2
sa35577 <38817928+sa35577@users.noreply.github.com>
2023-12-30 15:20:30 +0000

    Sync LeetCode submission - LRU Cache (cpp)

--- solutions/problems/lru_cache/solution.cpp
+++ solutions/problems/lru_cache/solution.cpp
@@ -1,54 +1,56 @@
 class LRUCache {
 public:
+    int lru[10005];
+    int time;
+    int c;
+    int op[400005];
+    int kick;
+    int vals[100005];
     int sz;
-    int pushFront;
-    int arr[400004];
-    int nxt;
-    pair<int,int> kv[10005]; //pair < value associted with key, index in ordering of arr >
-    int cap;
+
     LRUCache(int capacity) {
+        c = capacity;
+        time = 0;
+        fill(lru,lru+10005,0);
+        fill(op,op+400005,0);
+        fill(vals,vals+100005,-1);
+        kick = 0;
         sz = 0;
-        pushFront = 0;
-        nxt = 0;
-        for (int i = 0; i < 10005; i++) kv[i] = make_pair(-1,-1);
-        for (int i = 0; i < 200004; i++) arr[i] = -1;
-        cap = capacity;
     }
     
     int get(int key) {
-        int res =  kv[key].first;
-        if (res == -1) return -1;
-        arr[kv[key].second] = -1;
-        kv[key].second = nxt;
-        arr[nxt] = key;
-        nxt++;
-
-        // for (int i = 1; i < 10; i++) {
-        //     cout << kv[i].first << " ";
-        // }
-        // cout << endl;
-        return res;
+        if (vals[key] == -1) return -1;
+        time++;
+        lru[key] = time;
+        op[time] = key;
+        return vals[key];
     }
     
     void put(int key, int value) {
-        if (kv[key].first != -1) {
-            kv[key].first = value;
-            arr[kv[key].second] = -1;
-            kv[key].second = nxt;
+        time++;
+        lru[key] = time;
+        op[time] = key;
+
+        if (vals[key] != -1) { //update if already in cache
+            vals[key] = value;
         }
-        else if (sz >= cap) {
-            while (arr[pushFront] == -1) pushFront++;
-            kv[arr[pushFront]] = make_pair(-1,-1);
-            pushFront++;
-            kv[key] = make_pair(value,nxt);
-        }
-        else {
-            kv[key] = make_pair(value,nxt);
+
+        else if (sz < c) { //if room in cache, simply add to cache
             sz++;
+            vals[key] = value;
+            
         }
-        arr[nxt] = key;
-        nxt++;
-        
+        else { //kick
+            while (true) {
+                kick++;
+                if (lru[op[kick]] == kick) {
+                    break;
+                }
+            }
+            //kick number in op[kick]
+            vals[op[kick]] = -1;
+            vals[key] = value;
+        }
     }
 };
 
