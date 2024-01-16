fe024dcc770015136d0c8ac47091dba1b4c4934f
sa35577 <38817928+sa35577@users.noreply.github.com>
2024-01-16 15:46:09 +0000

    Sync LeetCode submission - Flower Planting With No Adjacent (cpp)

--- solutions/problems/flower_planting_with_no_adjacent/solution.cpp
+++ solutions/problems/flower_planting_with_no_adjacent/solution.cpp
@@ -1,38 +1,21 @@
 class Solution {
 public:
     vector<int> gardenNoAdj(int n, vector<vector<int>>& paths) {
-        vector<bool> vis(n+1);
-        vector<vector<int>> graph(n+1);
+        vector<vector<int>> graph(n);
         for (vector<int> v : paths) {
-            graph[v[0]].push_back(v[1]);
-            graph[v[1]].push_back(v[0]);
-            
+            graph[v[0]-1].push_back(v[1]-1);
+            graph[v[1]-1].push_back(v[0]-1);
         }
-        fill(vis.begin(),vis.end(),false);
-        vector<int> answer(n);
-        fill(answer.begin(),answer.end(),0);
-        for (int idx = 1; idx <= n; idx++) {
-            if (answer[idx-1] == 0) {
-                //BFS
-                deque<int> deck;
-                deck.push_back(idx);
-                while (!deck.empty()) {
-                    int cur = deck.front();
-                    deck.pop_front();
-                    if (vis[idx]) continue;
-                    vis[idx] = 1;
-                    bool occ[5];
-                    for (int i = 0; i < 5; i++) occ[i] = false;
-                    for (int i : graph[cur]) {
-                        if (vis[i]) occ[answer[i-1]] = true;
-                        else deck.push_back(i);
-                    }
-                    int colour = 1;
-                    while (occ[colour]) colour++;
-                    answer[idx-1] = colour;   
-                }
-            }
+        vector<int> colour(n);
+        fill(colour.begin(),colour.end(),0);
+        for (int i = 0; i < colour.size(); i++) {
+            set<int> st;
+            for (int x : graph[i]) st.insert(colour[x]);
+            if (st.find(1) == st.end()) colour[i] = 1;
+            else if (st.find(2) == st.end()) colour[i] = 2;
+            else if (st.find(3) == st.end()) colour[i] = 3;
+            else if (st.find(4) == st.end()) colour[i] = 4;
         }
-        return answer;
+        return colour;
     }
 };
