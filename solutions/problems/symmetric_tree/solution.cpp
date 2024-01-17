c73295f403697ae198fed0640384a91fd9cf87b5
sa35577 <38817928+sa35577@users.noreply.github.com>
2024-01-17 16:34:39 +0000

    Sync LeetCode submission - Symmetric Tree (cpp)

--- solutions/problems/symmetric_tree/solution.cpp
+++ solutions/problems/symmetric_tree/solution.cpp
@@ -11,32 +11,15 @@
  */
 class Solution {
 public:
-    vector<int> travLeft(TreeNode* root) {
-        if (root == nullptr) return {-1000};
-        vector<int> v{root->val};
-        vector<int> leftRec = travLeft(root->left), rightRec = travLeft(root->right);
-        v.insert(v.end(),leftRec.begin(),leftRec.end());
-        v.insert(v.end(),rightRec.begin(),rightRec.end());
-        return v;
-    }
-
-    vector<int> travRight(TreeNode* root) {
-        if (root == nullptr) return {-1000};
-        vector<int> v{root->val};
-        vector<int> leftRec = travRight(root->left), rightRec = travRight(root->right);
-        v.insert(v.end(),rightRec.begin(),rightRec.end());
-        v.insert(v.end(),leftRec.begin(),leftRec.end());
-        return v;
-    }
-
-
-
-    bool isSymmetric(TreeNode* root) {
-        vector<int> v1 = travLeft(root), v2 = travRight(root);
-        if (v1.size() != v2.size()) return false;
-        for (int i = 0; i < v1.size(); i++) {
-            if (v1[i] != v2[i]) return false;
-        }
+    bool sym(TreeNode* left, TreeNode* right) {
+        if (left == nullptr && right == nullptr) return true;
+        if (left == nullptr || right == nullptr) return false;
+        if (left->val != right->val) return false;
+        if (!sym(left->right,right->left)) return false;
+        if (!sym(left->left,right->right)) return false;
         return true;
+    }
+    bool isSymmetric(TreeNode* root) {
+        return sym(root->left,root->right);
     }
 };
