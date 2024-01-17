f8c1d40954b648da5d3cefca0136a9966f14913a
sa35577 <38817928+sa35577@users.noreply.github.com>
2024-01-17 16:37:20 +0000

    Sync LeetCode submission - Word Break (cpp)

--- solutions/problems/word_break/solution.cpp
+++ solutions/problems/word_break/solution.cpp
@@ -1,19 +1,18 @@
 class Solution {
 public:
     bool wordBreak(string s, vector<string>& wordDict) {
-        bool getHere[301];
-        getHere[0] = 1;
-        for (int i = 1; i <= 300; i++) getHere[i] = 0;
+        vector<bool> dp(s.size()+1);
+        fill(dp.begin(),dp.end(),false);
+        dp[0] = true;
         for (int i = 0; i < s.size(); i++) {
-            //cout << i << " " << getHere[i] << "\n";
-            if (getHere[i]) {
+            if (dp[i]) {
                 for (string word : wordDict) {
-                    if (i + word.size() <= s.size() && s.substr(i,word.size()) == word) {
-                        getHere[i+word.size()] = true;
+                    if (i + word.size() < dp.size() && s.substr(i,word.size()) == word) {
+                        dp[i + word.size()] = true;
                     }
                 }
             }
         }
-        return getHere[s.size()];
+        return dp[s.size()];
     }
 };
