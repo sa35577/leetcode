e6d0738f7a625871e41ddc414770c386bd952f6b
sa35577 <38817928+sa35577@users.noreply.github.com>
2024-01-17 16:50:41 +0000

    Sync LeetCode submission - Snakes and Ladders (cpp)

--- solutions/problems/snakes_and_ladders/solution.cpp
+++ solutions/problems/snakes_and_ladders/solution.cpp
@@ -2,48 +2,37 @@ class Solution {
 public:
     int snakesAndLadders(vector<vector<int>>& board) {
         int n = board.size();
-        vector<int> v(n*n+1);
-        v[0] = 0;
-        int cnt = 1;
+        vector<int> b{-1};
+        
         for (int row = n-1; row >= 0; row--) {
-            if (((n-1) - row) % 2 == 0) { //moving right
-                for (int col = 0; col < n; col++) {
-                    v[cnt++] = board[row][col];
-                }
+            if (((n-1) - row) % 2 == 0) {
+                //sweep right
+                for (int col = 0; col < n; col++) b.push_back(board[row][col]);
             }
-            else { //moving left
-                for (int col = n-1; col >= 0; col--) {
-                    v[cnt++] = board[row][col];
-                }
+            else {
+                //sweep left
+                for (int col = n-1; col >= 0; col--) b.push_back(board[row][col]);
             }
         }
-        // cout << "done\n";
-        vector<int> mn(n*n+1);
-        fill(mn.begin(),mn.end(),100000);
-        // mn[1] = 0;
+        vector<int> minDist(b.size());
+        fill(minDist.begin(),minDist.end(),INT_MAX);
         priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> pq;
         pq.push({0,1});
+        minDist[1] = 0;
         while (!pq.empty()) {
             pair<int,int> cur = pq.top();
+            int dist = cur.first, node = cur.second;
             pq.pop();
-            int dist = cur.first, pos = cur.second;
-            // cout << dist << " " << pos << endl;
-            if (mn[pos] > dist) {
-                mn[pos] = dist;
-                for (int i = pos+1; i <= min(n*n,pos+6); i++) {
-                    int nxt = i;
-                    if (v[nxt] != -1) nxt = v[nxt];
-                    if (mn[nxt] > dist+1) {
-                        pq.push({dist+1,nxt});
-                    }
+            for (int i = 1; i <= min(n*n - node,6); i++) {
+                int nxt = node + i;
+                if (b[nxt] != -1) nxt = b[nxt];
+                if (minDist[nxt] > dist + 1) {
+                    minDist[nxt] = dist + 1;
+                    pq.push({dist+1,nxt});
                 }
             }
         }
-        // for (int i = 1; i <= n*n; i++) {
-        //     cout << mn[i] << " ";
-        //     if (i % n == 0) cout << endl;
-        // }
-        if (mn[n*n] == 100000) return -1;
-        return mn[n*n];
+        if (minDist.back() == INT_MAX) return -1;
+        return minDist.back();
     }
 };
