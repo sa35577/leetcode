28dbeafeadbc013cea9dec36cf3dda552a0ec9a5
sa35577 <38817928+sa35577@users.noreply.github.com>
2024-01-18 21:44:52 +0000

    Sync LeetCode submission - Redundant Connection (cpp)

--- solutions/problems/redundant_connection/solution.cpp
+++ solutions/problems/redundant_connection/solution.cpp
@@ -1,68 +1,55 @@
 class Solution {
 public:
-
-    int dfs(vector<vector<pair<int,int>>> &adjlist, vector<int> &prev, vector<bool> &vis, int cur) {
-        cout << "Current vertex: " << cur << endl;
-        for (pair<int,int> nxt : adjlist[cur]) {
-            if (vis[nxt.first] && nxt.first != prev[cur]) {
-                prev[nxt.first] = cur;
-                return nxt.first;
-            }
-            else if (nxt.first != prev[cur]) {
-                prev[nxt.first] = cur;
-                vis[nxt.first] = 1;
-                int rec = dfs(adjlist,prev,vis,nxt.first);
-                if (rec != -1) return rec;
-            }
-        }
-        return -1;
-    }
-
-    vector<int> findRedundantConnection(vector<vector<int>>& edges) {
-        int n = -1;
-        for (vector<int> edge : edges) {
-            n = max(n,edge[0]);
-            n = max(n,edge[1]);
-        }
-        vector<bool> vis(n+1);
-        for (int i = 0; i <= n; i++) vis[i] = 0;
-        vector<vector<pair<int,int>>> adjlist;
-        adjlist.resize(n+1);
-        int cnt = 0;
-        for (vector<int> edge : edges) {
-            adjlist[edge[0]].push_back({edge[1],cnt});
-            adjlist[edge[1]].push_back({edge[0],cnt});
-            cnt++;
-        }
-
-
-
-        vis[1] = 1;
-        vector<int> prev;
-        prev.resize(n+1);
-        for (int i = 0; i <= n; i++) prev[i] = INT_MAX;
-        
-        int cyclicEnd = dfs(adjlist,prev,vis,1);
-
-        for (int i = 1; i < prev.size(); i++) cout << i << ": " << prev[i] << endl;
-
-        int mx = -1;
-        int cur = cyclicEnd;
-        while (!(cur == cyclicEnd && mx > -1)) {
-            cout << cur;
-            int prevVertex = prev[cur];
-
-            for (int i = 0; i < edges.size(); i++) {
-                if ((edges[i][0] == cur && edges[i][1] == prevVertex) || (edges[i][0] == prevVertex && edges[i][1] == cur)) {
-                    mx = max(mx,i);
-                    break;
+    int repeatingVertex = -1;
+    set<pair<int,int>> st;
+    bool dfs(vector<vector<int>> &graph, vector<bool>& vis, int n, int v, int prev) {
+        vis[v] = true;
+        for (int nxt : graph[v]) {
+            if (nxt != prev) {
+                if (vis[nxt]) {
+                    repeatingVertex = nxt;
+                    st.insert({nxt,v});
+                    st.insert({v,nxt});
+                    return true;
+                }
+                else {
+                    if (dfs(graph,vis,n,nxt,v)) {
+                        if (repeatingVertex != -1) {
+                            st.insert({nxt,v});
+                            st.insert({v,nxt});
+                            if (repeatingVertex == v) {
+                                repeatingVertex = -1;
+                            }
+                        } 
+                        return true;
+                    }
                 }
             }
-
-            cur = prevVertex;
         }
-        return edges[mx];
-
-        
+        vis[v] = false;
+        return false;
+    }
+    vector<int> findRedundantConnection(vector<vector<int>>& edges) {
+        // graph has n-1 edges if n vertices
+        // therefore edges.size() = n
+        int n = edges.size();
+        vector<vector<int>> graph(n+1);
+        for (vector<int> &edge : edges) {
+            graph[edge[0]].push_back(edge[1]);
+            graph[edge[1]].push_back(edge[0]);
+        }
+        queue<int> q;
+        q.push(1);
+        vector<bool> vis(n+1);
+        fill(vis.begin(),vis.end(),false);
+        dfs(graph,vis,n,1,-1);
+        reverse(edges.begin(),edges.end());
+        for (vector<int> edge : edges) {
+            pair<int,int> p;
+            p.first = edge[0];
+            p.second = edge[1];
+            if (st.find(p) != st.end()) return edge;
+        }
+        return {-1,-1};
     }
 };
