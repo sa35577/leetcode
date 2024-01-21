32c6c3114eeefa4daa907db4b9e7a17c77d46c84
sa35577 <38817928+sa35577@users.noreply.github.com>
2024-01-21 22:57:28 +0000

    Sync LeetCode submission - House Robber (cpp)

--- solutions/problems/house_robber/solution.cpp
+++ solutions/problems/house_robber/solution.cpp
@@ -1,14 +1,10 @@
 class Solution {
 public:
     int rob(vector<int>& nums) {
-        if (nums.size() == 1) return nums[0];
-        vector<int> dp(nums.size());
-        dp[0] = nums[0];
-        dp[1] = max(dp[0],nums[1]);
-        for (int i = 2; i < dp.size(); i++) {
-            dp[i] = max(dp[i-1],dp[i-2]+nums[i]);
+        for (int i = 1; i < nums.size(); i++) {
+            if (i == 1) nums[i] = max(nums[i],nums[i-1]);
+            else nums[i] = max(nums[i-1],nums[i-2] + nums[i]);
         }
-        return dp.back();
-        
+        return nums.back();
     }
 };
