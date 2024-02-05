20bfb1c02a2ebcec1144b47463e7422ead24398c
sa35577 <38817928+sa35577@users.noreply.github.com>
2024-02-05 17:23:31 +0000

    Sync LeetCode submission - Subsets (cpp)

--- solutions/problems/subsets/solution.cpp
+++ solutions/problems/subsets/solution.cpp
@@ -1,21 +1,17 @@
 class Solution {
 public:
+    vector<vector<int>> getAll(vector<int>& nums, int idx, vector<int>& soFar) {
+        if (idx == nums.size()) return {soFar};
+        //pass forward skipping
+        vector<vector<int>> v1 = getAll(nums,idx+1,soFar);
+        soFar.push_back(nums[idx]);
+        vector<vector<int>> v2 = getAll(nums,idx+1,soFar);
+        soFar.pop_back();
+        v1.insert(v1.end(),v2.begin(),v2.end());
+        return v1;
+    }
     vector<vector<int>> subsets(vector<int>& nums) {
-        vector<vector<int>> v;
-        if (nums.size() == 0) {
-            vector<int> empty;
-            v.push_back(empty);
-            return v;
-        }
-        vector<int> rec = nums;
-        int val = nums[0];
-        rec.erase(rec.begin());
-        vector<vector<int>> subsetsRec = subsets(rec);
-        v.insert(v.end(),subsetsRec.begin(),subsetsRec.end());
-        for (vector<int> w : subsetsRec) {
-            w.push_back(val);
-            v.push_back(w);
-        }
-        return v;
+        vector<int> e{};
+        return getAll(nums,0,e);
     }
 };
