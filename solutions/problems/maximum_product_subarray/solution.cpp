3da6c41b7ee17b030851a113fa1f1a21fc292689
sa35577 <38817928+sa35577@users.noreply.github.com>
2024-02-06 16:54:36 +0000

    Sync LeetCode submission - Maximum Product Subarray (cpp)

--- solutions/problems/maximum_product_subarray/solution.cpp
+++ solutions/problems/maximum_product_subarray/solution.cpp
@@ -1,40 +1,46 @@
 class Solution {
 public:
-    int subArrayProd(vector<int>& nums, int L, int R) {
-        if (L > R) return 0;
-        int res = 1;
-        for (int i = L; i <= R; i++) res *= nums[i];
-        return res;
-    }
-    
     int maxProduct(vector<int>& nums) {
-        if (nums.size() == 1) return nums.back();
-        vector<int> zeroidx{-1};
-        int mx = INT_MIN;
+        // find intervals of nonzero entries. if all entries are 0, then return 0
+        // say we have a window [L,R] of nonzero entries. since these are all numbers, taking the product of everything will maximize the overall product of the inside. if this product is positive, this is the best we can do. if it is not, then we need to check the first and last negative #, say at position l1 and r1. we need to take max of [L,l1-1], [l1+1,R], [L,l2-1], [l2+1,R].
+        bool allneg = true;
+        vector<int> zeros{-1};
         for (int i = 0; i < nums.size(); i++) {
-            if (nums[i] == 0) zeroidx.push_back(i);
-            mx = max(mx,nums[i]);
+            if (nums[i] == 0) zeros.push_back(i);
         }
-        zeroidx.push_back(nums.size());
-        
-        for (int i = 1; i < zeroidx.size(); i++) {
-            int L = zeroidx[i-1]+1;
-            int R = zeroidx[i]-1;
-            
+        zeros.push_back(nums.size());
+        int best = INT_MIN;
+        if (zeros.size() > 2) best = 0;
+        for (int idx = 1; idx < zeros.size(); idx++) {
+            int L = zeros[idx-1]+1, R = zeros[idx]-1;
+            if (L > R) continue;
             int cntneg = 0;
-            for (int i = L; i <= R; i++) if (nums[i] < 0) cntneg++;
+            for (int i = L; i <= R; i++) {
+                if (nums[i] < 0) cntneg++;
+            }
+            // cout << cntneg << endl;
             if (cntneg % 2 == 0) {
-                mx = max(mx,subArrayProd(nums,L,R));
+                int v = 1;
+                for (int i = L; i <= R; i++) v *= nums[i];
+                best = max(best,v);
+            }
+            else if (L == R) {
+                best = max(best,nums[L]);
             }
             else {
-                int leftptr = L, rightptr = R;
-                while (nums[leftptr] > 0) leftptr++;
-                while (nums[rightptr] > 0) rightptr--;
-                mx = max(mx,subArrayProd(nums,leftptr+1,R));
-                mx = max(mx,subArrayProd(nums,L,rightptr-1));
+                //there is at least 2 integers
+                int firstneg = L, lastneg = R;
+                while (nums[firstneg] > 0) firstneg++;
+                while (nums[lastneg] > 0) lastneg--;
+                int v1 = 1,v2 = 1, v3 = 1, v4 = 1;
+                for (int i = L; i < firstneg; i++) v1 *= nums[i];
+                for (int i = firstneg+1; i <= R; i++) v2 *= nums[i];
+                for (int i = L; i < lastneg; i++) v3 *= nums[i];
+                for (int i = lastneg+1; i <= R; i++) v4 *= nums[i];
+                best = max(best,max(max(v1,v2),max(v3,v4)));
             }
         }
-        return mx;
+        return best;
+        
     }
-    
 };
