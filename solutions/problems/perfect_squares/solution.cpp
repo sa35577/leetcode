810e6bdfb8263162aba49c0d9e8184a3fc1c30f9
sa35577 <38817928+sa35577@users.noreply.github.com>
2024-02-08 17:30:00 +0000

    Sync LeetCode submission - Perfect Squares (cpp)

--- solutions/problems/perfect_squares/solution.cpp
+++ solutions/problems/perfect_squares/solution.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
     int numSquares(int n) {
-        int sq[102];
-        for (int i = 0; i < 102; i++) sq[i] = i*i;
+        vector<int> sq;
+        for (int i = 1; i <= 100; i++) sq.push_back(i*i);
         vector<int> dp(n+1);
-        for (int i = 1; i <= n; i++) dp[i] = 100000;
+        fill(dp.begin(),dp.end(),100000);
         dp[0] = 0;
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < 102; j++) {
-                if (i+sq[j] <= n && dp[i+sq[j]] > dp[i]+1) {
-                    dp[i+sq[j]] = dp[i]+1;
-                }
+        for (int i = 1; i < dp.size(); i++) {
+            for (int &x : sq) {
+                if (i - x < 0) break;
+                dp[i] = min(dp[i],dp[i-x]+1);
             }
         }
         return dp.back();
+
     }
 };
